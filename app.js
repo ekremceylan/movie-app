9aba9866fa3fbd3caa139fc3ea69316372c252df
EKREM CEYLAN <159719929+ekremceylan@users.noreply.github.com>
2024-09-06 23:40:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 
-  // const API_KEY = "fa11533dc287e0328c693aac9ffe5461";
-  // const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=1`;
-  // const IMG_PATH = `https://image.tmdb.org/t/p/w1280`;
-  // const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
-
-
   const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=702327d8f5a8e8a2fb179c1ededa7168&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?api_key=702327d8f5a8e8a2fb179c1ededa7168&query=";
 
-
   const form = document.getElementById("form");
   const search = document.getElementById("search");
   const main = document.getElementById("main");
-
 
 getMovies(API_URL);
 
